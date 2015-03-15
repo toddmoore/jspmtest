09167485d367fc217488a777d4d2ec4d4330da66
Todd Moore <moore@soapcreative.com>
2015-03-15 19:44:04 +1100

    0.1.7

--- sauce_labs_capabilities.js
+++ sauce_labs_capabilities.js
@@ -1,20 +1,20 @@
-var pkg = require('./package.json');
+sevar pkg = require('./package.json');
 
 module.exports.capabilities = {
-  sl_firefox: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'Linux',
-    version: '14',
-    name: pkg.name + " (build: " + process.env.TRAVIS_BUILD_NUMBER + ")"
-  },
-  sl_ios_safari: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.8',
-    version: '6',
-    name: pkg.name + " (build: " + process.env.TRAVIS_BUILD_NUMBER + ")"
-  }//,
+  // sl_firefox: {
+  //   base: 'SauceLabs',
+  //   browserName: 'firefox',
+  //   platform: 'Linux',
+  //   version: '14',
+  //   name: pkg.name + " (build: " + process.env.TRAVIS_BUILD_NUMBER + ")"
+  // },
+  // sl_ios_safari: {
+  //   base: 'SauceLabs',
+  //   browserName: 'safari',
+  //   platform: 'OS X 10.8',
+  //   version: '6',
+  //   name: pkg.name + " (build: " + process.env.TRAVIS_BUILD_NUMBER + ")"
+  // }//,
   // sl_ie_9: {
   //   base: 'SauceLabs',
   //   browserName: 'internet explorer',
